4725a621b185daa43dc7ef41c5da83b5cc69672e
Ryan Kelly <ryan@rfk.id.au>
2005-03-03 12:07:49 +0000

    Adding a utils.py module with some ideas for higher-level functionality (no code yet tho...)

---
+++ enchant/utils.py
@@ -0,0 +1,55 @@
+# pyenchant
+#
+# Copyright (C) 2004 Ryan Kelly
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+#
+# In addition, as a special exception, you are
+# given permission to link the code of this program with
+# non-LGPL Spelling Provider libraries (eg: a MSFT Office
+# spell checker backend) and distribute linked combinations including
+# the two.  You must obey the GNU Lesser General Public License in all
+# respects for all of the code used other than said providers.  If you modify
+# this file, you may extend this exception to your version of the
+# file, but you are not obligated to do so.  If you do not wish to
+# do so, delete this exception statement from your version.
+#
+"""
+
+    enchant.utils:    Misc utilities for the enchant package
+    
+    This module provies miscellaneous utilities for use with the
+    enchant spellchecking package.  Currently, it's sort of a
+    'play area' to try out new ideas.  It may stabilise into
+    a useful set of tools in the near future.
+    
+    Current things to be implemented:
+        
+        * a stateful SpellChecker class which can be used as a backend
+          to a standard dialog.  It would have things like ignore once,
+          ignore always, replace always and so forth.
+          
+        * a tokeniser, to break paragraphs into individual words for
+          spellchecking purposes.  We might be able to find an
+          existing implementation...
+          
+        * perhaps some GUIs implemented in the common toolkits, e.g.
+          Tkinter and wxPython, as examples of how to use it as well
+          as for general use.
+          
+"""
+
+
