1782b47a8fccaa5e24304d211f1fc9377ca78fd3
Ryan Kelly <ryan@rfk.id.au>
2005-03-20 01:12:36 +0000

    Added LGPL header to the checker package files

--- enchant/checker/__init__.py
+++ enchant/checker/__init__.py
@@ -1,3 +1,37 @@
+# pyenchant
+#
+# Copyright (C) 2004-2005, Ryan Kelly
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+#
+# In addition, as a special exception, you are
+# given permission to link the code of this program with
+# non-LGPL Spelling Provider libraries (eg: a MSFT Office
+# spell checker backend) and distribute linked combinations including
+# the two.  You must obey the GNU Lesser General Public License in all
+# respects for all of the code used other than said providers.  If you modify
+# this file, you may extend this exception to your version of the
+# file, but you are not obligated to do so.  If you do not wish to
+# do so, delete this exception statement from your version.
+#
+"""
+
+    TODO: document the enchant.checker module
+
+"""
 
 import array
 
@@ -6,11 +40,7 @@ from enchant.tokenize import get_tokenizer
 
 class SpellChecker:
     """Class implementing stateful spellchecking behavior.
-    
-    NOTE:  this class will eventually be moved out of the
-           enchant.utils package into a special-purpose
-           module. It lives in utils only while in development
-    
+        
     This class is designed to implement a spell-checking loop over
     a block of text, correcting/ignoring/replacing words as required.
     This loop is implemented using an iterator paradigm so it can be
