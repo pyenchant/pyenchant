1c17a7ded02e7dc21d0a4d4d826d3a680661990e
Ryan Kelly <ryan@rfk.id.au>
2009-01-08 12:24:25 +1100

    Version bump for 1.5.1 release

--- enchant/__init__.py
+++ enchant/__init__.py
@@ -79,7 +79,7 @@
 # Make version info available
 __ver_major__ = 1
 __ver_minor__ = 5
-__ver_patch__ = 0
+__ver_patch__ = 1
 __ver_sub__ = ""
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,
                               __ver_patch__,__ver_sub__)
@@ -1166,7 +1166,6 @@ class TestInstallEnv(unittest.TestCase):
         self._insDir = raw_unicode(r'test_\xe5\xe4\xf6_ing')
         self.install()
         self.runtests()
-
     
 
 def testsuite(recurse=True):
