c5b5c266f45b4e2f914fe294a64b4cb9ca97c6ce
Ryan Kelly <ryan@rfk.id.au>
2009-05-02 15:32:19 +1000

    bump version number in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import shutil
 WINDEPS = ".\\tools\\pyenchant-bdist-win32-sources\\build"
 
 #  Cant seem to obtain version information from module, must hardcode
-VERSION = "1.5.2"
+VERSION = "1.5.3"
 
 # Package MetaData
 NAME = "pyenchant"
