6b468f48495d1b947a9180bbf8c160c7146de925
Ryan Kelly <ryan@rfk.id.au>
2010-03-06 18:36:34 +1100

    bump to v0.6.1

--- enchant/__init__.py
+++ enchant/__init__.py
@@ -80,7 +80,7 @@ _DOC_ERRORS = ['enchnt','enchnt', 'fr']
 # Make version info available
 __ver_major__ = 1
 __ver_minor__ = 6
-__ver_patch__ = 0
+__ver_patch__ = 1
 __ver_sub__ = ""
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,
                               __ver_patch__,__ver_sub__)
