58140811b97696a762461cecafee9dc4cf7d8e75
Ryan Kelly <ryan@rfk.id.au>
2011-07-02 09:02:26 +1000

    let trim_suggestions take an arbitrary distance function

--- enchant/utils.py
+++ enchant/utils.py
@@ -228,14 +228,21 @@ def levenshtein(s1, s2):
 
 
 
-def trim_suggestions(word,suggs,maxlen):
+def trim_suggestions(word,suggs,maxlen,calcdist=None):
     """Trim a list of suggestions to a maximum length.
 
     If the list of suggested words is too long, you can use this function
     to trim it down to a maximum length.  It tries to keep the "best"
     suggestions based on similarity to the original word.
+
+    If the optional "calcdist" argument is provided, it must be a callable
+    taking two words and returning the distance between them.  It will be
+    used to determine which words to retain in the list.  The default is
+    a simple Levenshtein distance.
     """
-    decorated = [(levenshtein(word,s),s) for s in suggs]
+    if calcdist is None:
+        calcdist = levenshtein
+    decorated = [(calcdist(word,s),s) for s in suggs]
     decorated.sort()
     return [s for (l,s) in decorated[:maxlen]]
     
