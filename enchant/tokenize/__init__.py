442702586293dc0e3d29a06c710c22d68874a07d
Jakub Wilk <jwilk@jwilk.net>
2016-05-14 13:02:09 +0200

    Fix URLFilter regexp

--- enchant/tokenize/__init__.py
+++ enchant/tokenize/__init__.py
@@ -452,12 +452,12 @@ class URLFilter(Filter):
     """Filter skipping over URLs.
     This filter skips any words matching the following regular expression:
        
-           ^[a-zA-z]+:\/\/[^\s].*
+           ^[a-zA-Z]+:\/\/[^\s].*
         
     That is, any words that are URLs.
     """
     _DOC_ERRORS = ["zA"]
-    _pattern = re.compile(r"^[a-zA-z]+:\/\/[^\s].*")
+    _pattern = re.compile(r"^[a-zA-Z]+:\/\/[^\s].*")
     def _skip(self,word):
         if self._pattern.match(word):
             return True
