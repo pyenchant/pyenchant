e92eb1d169b55766374f976432b2dbb794157c66
Ryan Kelly <ryan@rfk.id.au>
2017-12-10 11:19:42 +1100

    Prep v2.0.0

--- enchant/__init__.py
+++ enchant/__init__.py
@@ -79,9 +79,9 @@ of 'Error'.
 _DOC_ERRORS = ['enchnt','enchnt','incant','fr']
 
 # Make version info available
-__ver_major__ = 1
-__ver_minor__ = 6
-__ver_patch__ = 11
+__ver_major__ = 2
+__ver_minor__ = 0
+__ver_patch__ = 0
 __ver_sub__ = ""
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,
                               __ver_patch__,__ver_sub__)
