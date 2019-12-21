00bd65a0e23c916527a8cfed3558fba762f3e2be
Dimitri Merejkowsky <d.merej@gmail.com>
2019-12-21 15:28:40 +0100

    Convert failing script to working test :)

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -1,24 +1,19 @@
-import os
 import enchant
 from multiprocessing import Pool
 
 
-def do_something(words):
+def check_words(words):
     d = enchant.Dict("en-US")
     for word in words:
         d.check(word)
     return True
 
 
-def will_block():
+def test_can_use_multiprocessing():
     words = ["hello" for i in range(1000)]
     input = [words for i in range(1000)]
     print("Starting")
     pool = Pool(10)
-    for i, result in enumerate(pool.imap_unordered(do_something, input)):
+    for i, result in enumerate(pool.imap_unordered(check_words, input)):
         print("Done {0}: {1}".format(i, result))
     print("Finished")
-
-
-if __name__ == "__main__":
-    will_block()
