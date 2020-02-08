759c5a55f162a7f446056ea71f3d578e603623d3
Dimitri Merejkowsky <d.merej@gmail.com>
2020-02-08 20:35:53 +0100

    Add a release script and tox env
    We need build 3 wheels and a src dist, but we can do it on any
    platform :)

--- bootstrap.py
+++ bootstrap.py
@@ -11,30 +11,30 @@ ENCHANT_TAG = "v2.2.7-appveyor-35"
 DOWNLOAD_URL = "https://github.com/pyenchant/enchant/releases/download/"
 
 
-def bootstrap_windows():
-    bits, linkage = platform.architecture()
-    if linkage != "WindowsPE":
-        sys.exit("Unsupported platform: " + linkage)
-    if bits == "32bit":
-        bits = "32"
-    elif bits == "64bit":
-        bits = "64"
-    else:
-        sys.exit("Unsupported number of bits: ", bits)
-    artifact_url = DOWNLOAD_URL + ENCHANT_TAG + "/enchant-prefix" + "-" + bits + ".zip"
+def bootstrap_windows(platform):
+    bits = {"win32": "32", "win_amd64": "64"}[platform]
+    archive_name = "enchant-prefix" + "-" + bits + ".zip"
+    artifact_url = DOWNLOAD_URL + ENCHANT_TAG + "/" + archive_name
     print("Retrieving artifact from", artifact_url, "...")
     response = requests.get(artifact_url, stream=True)
-    archive_path = "enchant-prefix.zip"
-    with open(archive_path, "wb") as f:
+    with open(archive_name, "wb") as f:
         shutil.copyfileobj(response.raw, f)
     data_path = "enchant/data/"
     print("Unpacking artifact to", data_path, "...")
-    shutil.unpack_archive(archive_path, data_path)
+    shutil.unpack_archive(archive_name, data_path)
 
 
 def main():
     if sys.platform == "win32":
-        bootstrap_windows()
+        if linkage != "WindowsPE":
+            sys.exit("Unsupported platform: " + linkage)
+        if bits == "32bit":
+            pltaform = "win32"
+        elif bits == "64bit":
+            bits = "wim_amd64"
+        else:
+            sys.exit("Unsupported number of bits: ", bits)
+        bootstrap_windows(platform)
 
 
 if __name__ == "__main__":
---
+++ release.py
@@ -0,0 +1,58 @@
+import os
+import platform
+import shutil
+import subprocess
+import sys
+
+
+from bootstrap import bootstrap_windows
+
+
+def run(*cmd):
+    print(*cmd)
+    subprocess.run(cmd, check=True, stdout=subprocess.PIPE)
+
+
+def ensure_empty(data_path):
+    print("Ensuring that", data_path, "is empty")
+    if os.path.exists(data_path):
+        shutil.rmtree(data_path)
+    os.mkdir(data_path)
+
+
+def make_windows_wheel(platform):
+    ensure_empty("build/")
+    ensure_empty("enchant/data/")
+    bootstrap_windows(platform)
+    run("python", "setup.py", "bdist_wheel", "--plat-name", platform)
+
+
+def make_wheel_any():
+    ensure_empty("build/")
+    ensure_empty("enchant/data/")
+    run("python", "setup.py", "bdist_wheel")
+
+
+def make_sdist():
+    ensure_empty("build/")
+    ensure_empty("enchant/data/")
+    run("python", "setup.py", "sdist")
+
+
+def main():
+    """ Build artifacts that we need to upload to  pypi """
+    ensure_empty("build/")
+    ensure_empty("dist/")
+
+    print("Building artifacts for new release ...")
+    make_windows_wheel(platform="win_amd64")
+    make_windows_wheel(platform="win32")
+
+    make_wheel_any()
+    make_sdist()
+
+    print("Done!. Artifacts are in dist/, ready for upload")
+
+
+if __name__ == "__main__":
+    main()
