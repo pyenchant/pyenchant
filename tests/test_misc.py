aaa842435b49531537eef725a980ab5d27e230d4
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2020-05-20 11:01:49 +0200

    Fix running tests with old C enchant library

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -7,5 +7,15 @@ def test_get_version():
 
 
 def test_get_user_config_dir():
-    user_dir = enchant.get_user_config_dir()
-    print(user_dir)
+    """
+    Scenario:
+      Either broker.get_user_config_dir() works
+      (enchant >= 2.0), or it throws AttributeError
+      (enchant < 2.0)
+
+    """
+    try:
+        user_dir = enchant.get_user_config_dir()
+        print(user_dir)
+    except AttributeError:
+        pass
