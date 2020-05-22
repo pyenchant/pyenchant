d9fb956b6d80b3acfd00e90915000a763ec7a0db
Dimitri Merejkowsky <d.merej@gmail.com>
2020-05-22 11:53:43 +0200

    DictNotFound: add a link to the doc

--- enchant/__init__.py
+++ enchant/__init__.py
@@ -282,7 +282,8 @@ class Broker(_EnchantObject):
         self._check_this()
         new_dict = _e.broker_request_dict(self._this, tag.encode())
         if new_dict is None:
-            e_str = "Dictionary for language '%s' could not be found"
+            e_str = "Dictionary for language '%s' could not be found\n"
+            e_str += "Please check https://pyenchant.github.io/pyenchant/ for details"
             self._raise_error(e_str % (tag,), DictNotFoundError)
         if new_dict not in self._live_dicts:
             self._live_dicts[new_dict] = 1
