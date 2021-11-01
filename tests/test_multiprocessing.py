895b7289c6efc0590bf330fb06d8deaaeb746b10
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-11-01 15:49:59 +0100

    Disable tests that hangs on pypy + windows

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -1,5 +1,9 @@
-import enchant
+import sys
 from multiprocessing import Pool
+
+import pytest
+
+import enchant
 
 
 def check_words(words):
@@ -9,6 +13,10 @@ def check_words(words):
     return True
 
 
+@pytest.mark.skipif(
+    sys.implementation.name == "pypy" and sys.platform == "win32",
+    reason="hangs for an unknown reason",
+)
 def test_can_use_multiprocessing():
     words = ["hello" for i in range(1000)]
     input = [words for i in range(1000)]
